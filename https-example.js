d25d548874128e885ee226427d6f1448c591eb17
Peter Goshulak <pgoshulak@gmail.com>
2018-04-14 13:39:33 -0400

    Completed simple request to example.org

--- https-example.js
+++ https-example.js
@@ -1,2 +1,16 @@
 var https = require('https')
-console.log('required https!')
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+}
+
+var callback = () => {
+  console.log('in response handler callback')
+}
+
+console.log('About to make a request')
+
+https.request(options, callback).end()
+
+console.log('made the request')
